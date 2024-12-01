0438cf3368a8451db216ca54fc5705580a8e31c0
May <maykhine.ada@gmail.com>
2024-12-01 13:35:44 -0500

    add figma design & update on tokens

--- src/frontend/tokens.stylex.ts
+++ src/frontend/tokens.stylex.ts
@@ -1,11 +1,16 @@
 import * as stylex from "@stylexjs/stylex"
 
 export const tokens = stylex.defineVars({
-  offWhite: "#FFFEFB",
+  offWhite: "#FFFFFA",
   offWhiteGreen: "#eef4ed",
   darkestBlue: "#0b2545",
-  darkBlue: "#134074",
+  // darkBlue: "#134074",
   blue: "#8da9c4",
+  lightGreen: 'C0DFA1',
+  green: '9FC490',
+  tealGreen: '82A3A1',
+  grayTeal: '465362',
+  darkBlue: '465362'
 })
 
 export const stdStyles = stylex.create({
